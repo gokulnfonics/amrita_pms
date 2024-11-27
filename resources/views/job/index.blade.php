94460316af7f7a248cec3474fe9633e66061c8d3
gokulnfonics <gokul@nfonics.com>
2024-11-27 13:22:35 +0530

    pagination in job listing

--- resources/views/job/index.blade.php
+++ resources/views/job/index.blade.php
@@ -30,7 +30,7 @@
 					<!--begin::Table container-->
 					<div class="table-responsive">
 						<!--begin::Table-->
-						<table class="table table-row-dashed table-row-gray-300 align-middle gs-0 gy-4" id="campustable">
+						<table class="table table-row-dashed table-row-gray-300 align-middle gs-0 gy-4" id="jobtable">
 							<!--begin::Table head-->
 							<thead>
 								<tr class="fw-bold">
@@ -137,7 +137,7 @@
 
 <script>
 	$(document).ready(function() {
-		$('#departmenttable').DataTable({
+		$('#jobtable').DataTable({
 			"iDisplayLength": 10,
 			"searching": true,
 			"recordsTotal": 3615,
